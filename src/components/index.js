186b13520cacf80848b400d434fabaf017d13f14
Vishalisharwal <isharwalvishal@gmail.com>
2025-09-01 00:47:08 +0530

    Real Time Editor & Post-form

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,21 @@
 import Header from "./header/header";
 import Footer from "./footer/footer";
+import Container from "./container/container";
+import Button from "./Button";
+import Input from "./Input";
+import select from "./select";
+import Logo from "./logo";
+import LogoutBtn from "./header/LogoutBtn";
+import RTE from "./RTE";
 
 export {
     Header,
-    Footer
+    Footer,
+    Container,
+    Button,
+    Input,
+    select,
+    Logo,
+    LogoutBtn,
+    RTE
 }
